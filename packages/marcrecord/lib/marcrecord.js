65f6fe9cbb8f5813b601d4b2382dc8889e1d17be
Alexander Fronkin <alexander@fronkin.ru>
2013-12-07 01:54:20 +0400

    Added most of the data structures and methods Implemented some test suites

--- packages/marcrecord/lib/marcrecord.js
+++ packages/marcrecord/lib/marcrecord.js
@@ -3,7 +3,7 @@
  */
 
 /*
- * The MARC record constructor.
+ * The constructor of MARC record.
  */
 function MarcRecord() {
     // The record leader.
@@ -19,5 +19,149 @@ MarcRecord.prototype.size = function() {
     return this.fields.length;
 }
 
-// Export the class.
-module.exports = MarcRecord;
+/*
+ * Parses record data from the ISO2709 buffer.
+ */
+MarcRecord.prototype.parseIsoBuffer = function(buffer, encoding) {
+    // Validate function arguments.
+    encoding = encoding || 'utf8';
+
+    // Get the record leader.
+    this.leader = buffer.toString(encoding, 0, 24);
+}
+
+/*
+ * Adds variable field to the record.
+ */
+MarcRecord.prototype.addVariableField = function(variableField) {
+    this.fields.push(variableField);
+}
+
+/*
+ * Removes variable field from the record.
+ */
+MarcRecord.prototype.removeVariableField = function(variableField) {
+    var fieldNo = this.fields.indexOf(variableField);
+    if (fieldNo >= 0) {
+        this.fields.splice(fieldNo, 1);
+    }
+}
+
+/*
+ * Returns the first variable field with the given tag.
+ */
+MarcRecord.prototype.getVariableField = function(tag) {
+    for (var fieldNo in this.fields) {
+        var field = this.fields[fieldNo];
+        if (field.tag === tag) {
+            return field;
+        }
+    }
+    return null;
+}
+
+/*
+ * Returns a list of variable fields with the given tag.
+ */
+MarcRecord.prototype.getVariableFields = function(tag) {
+    var foundFields = [];
+    for (var fieldNo in this.fields) {
+        var field = this.fields[fieldNo];
+        if (field.tag === tag) {
+            foundFields.push(field);
+        }
+    }
+    return foundFields;
+}
+
+/*
+ * The constructor of MARC variable field (base field class).
+ */
+function MarcVariableField(tag) {
+    // Tag of the field.
+    this.tag = tag || null;
+}
+
+/*
+ * Returns 'true' if field is control field.
+ */
+MarcVariableField.prototype.isControlField = function() {
+    return this instanceof MarcControlField;
+}
+
+/*
+ * Returns 'true' if field is data field.
+ */
+MarcVariableField.prototype.isDataField = function() {
+    return this instanceof MarcDataField;
+}
+
+/*
+ * The constructor of MARC control field (subclass of the variable field).
+ */
+function MarcControlField(tag, data) {
+    // Call parent constructor.
+    MarcVariableField.call(this, tag);
+    // Data of the control field.
+    this.data = data || null;
+}
+
+MarcControlField.prototype = new MarcVariableField;
+
+/*
+ * The constructor of MARC data field (subclass of the variable field).
+ */
+function MarcDataField(tag, ind1, ind2) {
+    // Call parent constructor.
+    MarcVariableField.call(this, tag);
+    // Indicators.
+    this.ind1 = ind1 || ' ';
+    this.ind2 = ind2 || ' ';
+    // List of subfields of the data field. 
+    this.subfields = [];
+}
+
+MarcDataField.prototype = new MarcVariableField;
+
+/*
+ * Adds subfield to the field.
+ */
+MarcDataField.prototype.addSubfield = function(subfield) {
+    if (!(subfield instanceof MarcSubfield)) {
+        throw new Error('Wrong argument type');
+    }
+    this.subfields.push(subfield);
+}
+
+/*
+ * Returns number of subfields in the field.
+ */
+MarcDataField.prototype.size = function() {
+    return this.subfields.length;
+}
+
+/*
+ * The constructor of MARC subfield.
+ */
+function MarcSubfield(code, data) {
+    // The code of subfield.
+    this.code = code || null;
+    // The data of subfield.
+    this.data = data || null;
+}
+
+/*
+ * Returns 'true' if subfield is embedded field.
+ */
+MarcSubfield.prototype.isEmbeddedField = function() {
+    return this.data instanceof MarcVariableField;
+}
+
+// Export classes.
+var marcrecord = {
+    MarcRecord: MarcRecord,
+    MarcVariableField: MarcVariableField,
+    MarcControlField: MarcControlField,
+    MarcDataField: MarcDataField,
+    MarcSubfield: MarcSubfield };
+module.exports = marcrecord;
--- share/test/test1.js
+++ share/test/test1.js
@@ -1,5 +1,134 @@
-var MarcRecord = require('../../packages/marcrecord');
+var marcrecord = require('../../packages/marcrecord');
 
-// Create new MARC record.
-var record = new MarcRecord();
-console.log(record.size());
+// Create new record.
+process.stderr.write('Create new record: ');
+var record = new marcrecord.MarcRecord();
+if (record.size() === 0) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Create new control field.
+process.stderr.write('Create new control field: ');
+var controlField = new marcrecord.MarcControlField('001', 'ID/1');
+if (controlField.tag === '001' && controlField.data === 'ID/1') {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Add control field to the record.
+process.stderr.write('Add control field to the record: ');
+record.addVariableField(controlField);
+if (record.size() === 1) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Create new data field.
+process.stderr.write('Create new data field: ');
+var dataField = new marcrecord.MarcDataField('900', '1', '2');
+if (dataField.tag === '900' && dataField.ind1 == '1' && dataField.ind2 == '2'
+    && dataField.size() === 0)
+{
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Add data field to the record.
+process.stderr.write('Add data field to the record: ');
+record.addVariableField(dataField);
+if (record.size() === 2) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Create new subfield.
+process.stderr.write('Create new subfield: ');
+var subfield = new marcrecord.MarcSubfield('a', 'Subfield data');
+if (subfield.code === 'a' && subfield.data === 'Subfield data') {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Add subfield to the data field.
+process.stderr.write('Add subfield to the data field: ');
+dataField.addSubfield(subfield);
+if (dataField.size() === 1) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Create new embedded field.
+process.stderr.write('Create new embedded field: ');
+var embeddedField = new marcrecord.MarcControlField('001', 'ID/2');
+var subfield = new marcrecord.MarcSubfield('1', embeddedField);
+if (subfield.code === '1'
+    && subfield.data instanceof marcrecord.MarcControlField)
+{
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Add embedded field to the data field.
+process.stderr.write('Add embedded field to the data field: ');
+dataField.addSubfield(subfield);
+if (dataField.size() === 2) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+// Add another control field to the record and remove it right away.
+process.stderr.write('Add another control field to the record: ');
+var anotherControlField =
+    new marcrecord.MarcControlField('009', 'Control field');
+record.addVariableField(anotherControlField);
+record.removeVariableField(anotherControlField);
+if (record.size() === 2) {
+    process.stderr.write('OK\n');
+} else {
+    process.stderr.write('FAILED\n');
+}
+
+/*
+ * Print content of the field.
+ */
+function printField(field) {
+    if (field.isControlField()) {
+        process.stdout.write(field.tag);
+        if (field.data.length > 0) {
+            process.stdout.write(' ' + field.data);
+        }
+    } else if (field.isDataField()) {
+        process.stdout.write(field.tag + ' [' + field.ind1 + field.ind2 + ']');
+        for (var subfieldNo in field.subfields) {
+            var subfield = field.subfields[subfieldNo];
+
+            process.stdout.write(' $' + subfield.code);
+            if (!subfield.isEmbeddedField()) {
+                if (subfield.data.length > 0) {
+                    process.stdout.write(' ' + subfield.data);
+                }
+            } else {
+                printField(subfield.data);
+            }
+        }
+    } else {
+        process.stdout.write('Error: unknown field type.');
+    }
+}
+
+// Print content of the record.
+process.stderr.write('\nContent of the record:\n');
+for (var fieldNo in record.fields) {
+    printField(record.fields[fieldNo]);
+    process.stdout.write('\n');
+}
