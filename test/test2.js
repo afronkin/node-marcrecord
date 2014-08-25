7d1a414ad47fb113f9b9255e57ed6b78bd5828ad
Alexander Fronkin <alexander@fronkin.ru>
2014-08-25 23:31:34 +0400

    Added IsoWriter class

--- test/test2.js
+++ test/test2.js
@@ -3,31 +3,41 @@ var marcrecord = require('marcrecord');
 
 var MarcRecord = marcrecord.MarcRecord;
 var MarcIsoReader = marcrecord.MarcIsoReader;
+var MarcIsoWriter = marcrecord.MarcIsoWriter;
 
 // Open MARC file.
 var marcReader = new MarcIsoReader();
-marcReader.open('records_1.iso', 'cp1251', function(err) {
+marcReader.open('records_2.iso', 'cp1251', function(err) {
   if (err) {
     throw err;
   }
 
-  // Read MARC records from the file.
-  async.forever(
-    function(next) {
-      marcReader.next(function(err, record) {
-        if (err || !record) {
-          return callback(err || 'EOF');
-        }
-
-        console.log(record.toString());
-        next();
-      });
-    },
-    function(err) {
-      marcReader.close();
-      if (err && err !== 'EOF') {
-        console.error(err.message);
-      }
+  var marcWriter = new MarcIsoWriter();
+  marcWriter.open('records_3.iso', 'cp1251', function(err) {
+    if (err) {
+      throw err;
     }
-  );
+
+    // Read MARC records from the file.
+    async.forever(
+      function(next) {
+        marcReader.next(function(err, record) {
+          if (err || !record) {
+            return next(err || 'EOF');
+          }
+
+          marcWriter.write(record, next);
+        });
+      },
+      function(err) {
+        marcReader.close(function() {
+          marcWriter.close(function() {
+            if (err && err !== 'EOF') {
+              console.error(err.message);
+            }
+          });
+        });
+      }
+    );
+  });
 });
