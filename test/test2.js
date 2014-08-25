28ddf6ccb0afc5cfa3101c5cada3e6b43737f677
Alexander Fronkin <alexander@fronkin.ru>
2014-08-25 19:46:04 +0400

    Reworked test2.js

--- test/test2.js
+++ test/test2.js
@@ -1,37 +1,33 @@
-var fs = require('fs');
+var async = require('async');
+var marcrecord = require('marcrecord');
 
-var marcrecord = require('../'),
-    MarcRecord = marcrecord.MarcRecord,
-    MarcIsoReader = marcrecord.MarcIsoReader;
+var MarcRecord = marcrecord.MarcRecord;
+var MarcIsoReader = marcrecord.MarcIsoReader;
 
-/*
- * Reads a MARC records from the file.
- */
-function readNextRecord() {
-  marcReader.next(function(err, record) {
-    if (err) {
-      throw err;
-    }
-
-    // End of the file reached.
-    if (record === null) {
-      return;
-    }
-
-    // Print the content of the record.
-    process.stdout.write('Content of the record:\n');
-    process.stdout.write(record.toString());
-
-    // Read the next record after all I/O will be performed.
-    setTimeout(readNextRecord(), 0);
-  });
-}
-
-// Read a MARC record from the file.
+// Open MARC file.
 var marcReader = new MarcIsoReader();
-marcReader.open('records_2.iso', 'cp1251', function(err) {
+marcReader.open('records_1.iso', 'cp1251', function(err) {
   if (err) {
     throw err;
   }
-  readNextRecord();
+
+  // Read MARC records from the file.
+  async.forever(
+    function(next) {
+      marcReader.next(function(err, record) {
+        if (err || !record) {
+          return callback(err || 'EOF');
+        }
+
+        console.log(record.toString());
+        next();
+      });
+    },
+    function(err) {
+      marcReader.close();
+      if (err && err !== 'EOF') {
+        console.error(err.message);
+      }
+    }
+  );
 });
