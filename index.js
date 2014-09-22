58201bf096d96b0d7f63a01ba745e1114045425b
Alexander Fronkin <alexander@fronkin.ru>
2014-09-22 17:34:46 +0400

    Added isowriter to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var record = require('./lib/record');
 var field = require('./lib/field');
 var isoreader = require('./lib/isoreader');
+var isowriter = require('./lib/isowriter');
 var jsonreader = require('./lib/jsonreader');
 
 var exports = module.exports = {};
@@ -10,5 +11,6 @@ exports.MarcControlField = field.MarcControlField;
 exports.MarcDataField = field.MarcDataField;
 exports.MarcSubfield = field.MarcSubfield;
 exports.MarcIsoReader = isoreader.MarcIsoReader;
+exports.MarcIsoWriter = isowriter.MarcIsoWriter;
 exports.MarcJsonReader = jsonreader.MarcJsonReader;
 exports.parse = jsonreader.parseRecord;
