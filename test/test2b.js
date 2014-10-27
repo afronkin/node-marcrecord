312a316eeb458a115fb66db3391dfe79c266bd0b
Alexander Fronkin <alexander@fronkin.ru>
2014-10-27 14:03:54 +0300

    Added sync versions to the readers and writers.

---
+++ test/test2b.js
@@ -0,0 +1,18 @@
+var marcrecord = require('marcrecord');
+
+var MarcRecord = marcrecord.MarcRecord;
+var MarcIsoReader = marcrecord.MarcIsoReader;
+var MarcIsoWriter = marcrecord.MarcIsoWriter;
+
+var marcReader = new MarcIsoReader();
+marcReader.openSync('records_2.iso', 'cp1251');
+
+var marcWriter = new MarcIsoWriter();
+marcWriter.openSync('records_3.iso', 'cp1251');
+
+while (record = marcReader.nextSync()) {
+  marcWriter.writeSync(record);
+}
+
+marcReader.closeSync();
+marcWriter.closeSync();
