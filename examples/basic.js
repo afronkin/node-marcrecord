cbafb6e6e20550e75e32a8fa58a5d183c8e2aa27
Alexander Fronkin <alexander@fronkin.ru>
2014-10-31 22:14:30 +0200

    Added basic example. Updated README.

--- examples/basic.js
+++ examples/basic.js
@@ -1,13 +1,5 @@
-node-marcrecord
-===============
-MARC record library for Node.js allows to read and write records in ISO2709 and MARCXML
-containers and manipulate MARC record content (such as fields, subfields,
-record leader etc).
+var marcrecord = require('marcrecord');
 
-Implements similar API as C++ library "marcrecord".
-
-#Basic Example
-```javascript
 var marcReader = new marcrecord.MarcIsoReader();
 marcReader.openSync('records.iso');
 
@@ -23,8 +15,3 @@ while (record = marcReader.nextSync()) {
 }
 
 marcReader.closeSync();
-```
-
-Advanced Examples
--------------
-See the `test` and `examples` directories for more use cases.
